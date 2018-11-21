576a584d6b2970a7f872a3b10a9a0e625694ddbc
Michael Becco <e4e6d4d5@gmail.com>
2018-11-21 14:41:33 -0800

    Link Test- Hide Links When Not Admin User

---
+++ test/integration/site_links_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class SiteLinksTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:admin)
+  end
+
+  test 'navbar links guest user' do
+    get root_path
+    assert_select "a[href=?]", new_user_session_path
+    assert_select "a", count: 1
+  end
+
+  test 'navbar links admin user' do
+    sign_in(@user)
+    get root_path
+    assert_select "a[href=?]", destroy_user_session_path
+    assert_select "a[href=?]", daily_high_lows_path
+    assert_select "a[href=?]", new_stock_symbol_path
+    assert_select "a[href=?]", five_period_leaders_path
+    assert_select "a[href=?]", new_price_history_path
+    assert_select "a[href=?]", new_tradeable_universe_path
+    assert_select "a", count: 6
+  end
+end
