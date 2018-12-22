b476f608c913d0dd764891022dd55c0095035c23
Michael Becco <e4e6d4d5@gmail.com>
2018-12-22 12:39:14 -0800

    Modify Controller and View for Chartkick

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,5 +1,4 @@
 class ChartsController < ApplicationController
   def view
-    render json: MarketMonitor.group_by_day(:up_thirteen_pct_six_weeks).count
   end
 end
--- app/controllers/data_for_charts_controller.rb
+++ app/controllers/data_for_charts_controller.rb
@@ -1,4 +1,5 @@
 class DataForChartsController < ApplicationController
-  def thirteen_pct
+  def thirteen_percent
+    render json: MarketMonitor.group_by_day(:created_at).count
   end
 end
