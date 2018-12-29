a32467bc8152146ed908d797160747f60b962471
Michael Becco <e4e6d4d5@gmail.com>
2018-12-29 13:33:45 -0800

    Fix Bug With Incorrct Action

--- test/controllers/charts_controller_test.rb
+++ test/controllers/charts_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ChartsControllerTest < ActionDispatch::IntegrationTest
   test "should get thirteen_percent" do
-    get charts_thirteen_percent_url
+    get charts_view_path
     assert_response :success
   end
 
