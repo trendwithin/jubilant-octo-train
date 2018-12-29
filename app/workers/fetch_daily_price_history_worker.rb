a41f417dbf9c75d40f906a99780ffaf7b9cf531c
Michael Becco <e4e6d4d5@gmail.com>
2018-12-29 13:13:22 -0800

    Modify BarchartApiConnector to Inherit Modified MechanizeConnector, modify #parse_page_response_body_JSON

--- app/workers/fetch_daily_price_history_worker.rb
+++ app/workers/fetch_daily_price_history_worker.rb
@@ -2,7 +2,8 @@ class FetchDailyPriceHistoryWorker < ProcessWorker
 
   def perform
     daily_data_processor = BarchartDailyDataProcessor.new
-    fetch_daily_data = FetchDailyData.new daily_data_processor
+    api_connection = BarchartApiConnector.new
+    fetch_daily_data = FetchDailyData.new daily_data_processor, api_connection
     fetch_daily_data.collect_stock_symbols
     fetch_daily_data.fetch_daily_price_data
   end
--- app/workflows/barchart_api_connector.rb
+++ app/workflows/barchart_api_connector.rb
@@ -1,8 +1,11 @@
 require 'json'
 
 class BarchartApiConnector < MechanizeConnector
+  def initialize url = ''
+    super
+  end
 
   def parse_page_response_body_JSON
-    JSON.parse(agent.page.body)
+    JSON.parse(self.page.body)
   end
 end
