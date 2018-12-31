c209c46279340a1ff3119ccac210f90eae92ea0e
Michael Becco <e4e6d4d5@gmail.com>
2018-12-31 10:36:08 -0800

    Add Order and Api Endpoint Data

--- app/models/market_monitor.rb
+++ app/models/market_monitor.rb
@@ -12,4 +12,26 @@ class MarketMonitor < ApplicationRecord
   validates :up_fifty_pct_month, presence: true, numericality: { only_integer: true }
   validates :down_fifty_pct_month, presence: true, numericality: { only_integer: true }
   validates :total_stocks, presence: true, numericality: { only_integer: true }
+
+  def self.order_by_date_desc
+    order(:market_close_date)
+  end
+
+  def self.monitor
+    thirteen_percent_up = {"name":"Thirteen Pct Up"}
+    thirteen_percent_down = {"name":"Thirteen Pct Down"}
+
+    data_one = {}
+    data_two = {}
+
+    records = MarketMonitor.order_by_date_desc.limit(10)
+    data_one.merge! records.pluck(:market_close_date, :up_thirteen_pct_six_weeks).to_h
+    vals = { data: data_one }
+    thirteen_percent_up.merge!(vals)
+
+    data_two.merge! records.pluck(:market_close_date, :down_thirteen_pct_six_weeks).to_h
+    vals_two = { data: data_two}
+    thirteen_percent_down.merge!(vals_two)
+    @data = thirteen_percent_up, thirteen_percent_down
+  end
 end
