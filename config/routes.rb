95350e431b781a6734b2a936bc3eff896c07247f
Michael Becco <e4e6d4d5@gmail.com>
2018-12-31 10:35:16 -0800

    Add Endpoint for Chart

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
 
   namespace :data_for_charts do
     get 'thirteen_percent'
+    get 'monitor'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
