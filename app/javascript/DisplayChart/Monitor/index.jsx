886bc8a797d2c202fb0f1fc17484968299e9a5cd
Michael Becco <e4e6d4d5@gmail.com>
2019-01-02 10:35:01 -0800

    clean up code base

--- app/javascript/DisplayChart/Monitor/index.jsx
+++ app/javascript/DisplayChart/Monitor/index.jsx
@@ -7,47 +7,36 @@ class Monitor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [
-        // {"name":"Workout", "data": {"2017-01-01": 3, "2017-01-02": 4 }},
-        // {"name":"Call parents", "data": {"2017-01-01": 5, "2017-01-02": 3 }}
-      ],
+      data: [],
     };
   }
+
   componentDidMount() {
-    fetch("/data_for_charts/monitor")
+    fetch('/data_for_charts/monitor')
       .then(res => res.json())
       .then(
         (result) => {
-          // this.setState({
-          //   isLoaded: true,
-          //   items: result.items
-          // });
-          this.setState({
-            data: result,
-          })
-          console.log(result)
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+        this.setState({
+          data: result,
+        });
+      },
+
+      (error) => {
+        this.setState({
+          error,
+        });
+      }
+    );
   }
 
   render() {
     return (
       <div className='chart-display'>
         <LineChart data={'/data_for_charts/monitor'}
-                  discrete={false} />
+                   discrete={false} />
       </div>
     );
   }
 }
-
 
 export default Monitor;
