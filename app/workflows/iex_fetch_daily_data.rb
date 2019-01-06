4c48ebe1e4adbf1332d3cdd9577b1bcb61016194
Michael Becco <e4e6d4d5@gmail.com>
2019-01-05 17:28:02 -0800

    Test IEX w/ Valid and Invalid Quote

---
+++ app/workflows/iex_fetch_daily_data.rb
@@ -0,0 +1,16 @@
+class IexFetchDailyData
+  attr_reader :symbols_not_found
+  def initialize
+    @symbols_not_found = []
+  end
+
+  def quote symbol
+    begin
+      quote = IEX::Resources::Quote.get("#{symbol}")
+    rescue IEX::Errors::ClientError => iex_client_error
+    rescue IEX::Errors::SymbolNotFoundError => iex_symbol_not_found_error
+      symbols_not_found << symbol
+    end
+    quote
+  end
+end
