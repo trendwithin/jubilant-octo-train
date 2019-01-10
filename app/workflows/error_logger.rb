59b65338d35c627583707c50020efdfe1d4692e0
Michael Becco <e4e6d4d5@gmail.com>
2019-01-10 15:44:19 -0800

    ErrorLogger: Test Creating Error Log File to /log/custom_log

--- app/workflows/error_logger.rb
+++ app/workflows/error_logger.rb
@@ -18,4 +18,17 @@ class ErrorLogger
   def nil_symbols_report
     historic_data_errors[:symbols_return_nil]
   end
+
+  def error_data_file_exists? file_date, file_name
+    File.exists?(Rails.root.join('log', 'custom_log', "#{file_date}_" + "#{file_name}"))
+  end
+
+  def create_error_data_file file_date, file_name
+    file = "#{file_date}_#{file_name}"
+    Logger.new(File.join(Rails.root, 'log', 'custom_log', "#{file}"))
+  end
+
+  def format_prepend_date(current_time = Time.now)
+    current_time.strftime("%m_%d_%Y")
+  end
 end
--- test/workflows/error_logger_test.rb
+++ test/workflows/error_logger_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 require 'minitest/autorun'
 
 class ErrorLoggerTest < ActiveSupport::TestCase
+  setup do
+    @error_logger = ErrorLogger.new('logs')
+  end
+  
   test 'returns log' do
     log = {new_highs: {body: 'Report', from: 'New Highs', error: 'Failure'}}
     logger = ErrorLogger.new(log)
@@ -11,26 +15,61 @@ class ErrorLoggerTest < ActiveSupport::TestCase
 
   test '#nil_symbol with single symbol' do
     expected = {symbols_return_nil: ['NIL']}
-    error_logger = ErrorLogger.new 'logs'
-    error_logger.nil_symbol 'NIL'
-    result = error_logger.historic_data_errors
+    @error_logger.nil_symbol 'NIL'
+    result = @error_logger.historic_data_errors
     assert_equal expected, result
   end
 
   test '#nil_symbol with multiple symbols' do
     expected = {symbols_return_nil: ['NIL', 'VAL']}
-    error_logger = ErrorLogger.new 'logs'
-    error_logger.nil_symbol 'NIL'
-    error_logger.nil_symbol 'VAL'
-    result = error_logger.historic_data_errors
+    @error_logger.nil_symbol 'NIL'
+    @error_logger.nil_symbol 'VAL'
+    result = @error_logger.historic_data_errors
     assert_equal expected, result
   end
 
   test '#nil_symbols_report returns :symbols_return_nil' do
     expected = ['NIL']
-    error_logger = ErrorLogger.new 'logs'
-    error_logger.nil_symbol 'NIL'
-    result = error_logger.nil_symbols_report
+    @error_logger.nil_symbol 'NIL'
+    result = @error_logger.nil_symbols_report
     assert_equal expected, result
+  end
+
+  test '#format_prepend_date returns expcted date format' do
+    test_date = Date.new(2004, 10, 24)
+    expected = test_date.strftime("%m_%d_%Y")
+    value = @error_logger.format_prepend_date(test_date)
+    assert_equal expected, value
+  end
+
+  test 'existence of error log file' do
+    file_date = Date.new(2004, 10, 24).strftime("%m_%d_%Y")
+    file_name = 'nil_symbol_report.txt'
+    file = "#{file_date}_#{file_name}"
+    Logger.new(File.join(Rails.root, 'log', 'custom_log', "#{file}"))
+    assert @error_logger.error_data_file_exists?(file_date, file_name)
+  end
+
+  test 'non-existance of error log file' do
+    file_date = Date.new(2004, 10, 24).strftime("%m_%d_%Y")
+    file_name = 'nil_symbol_report.txt'
+    file = "#{file_date}_#{file_name}"
+    refute @error_logger.error_data_file_exists?(file_date, file_name)
+  end
+
+
+  test 'creat error file' do
+    file_date = Date.new(2004, 10, 24).strftime("%m_%d_%Y")
+    file_name = 'nil_symbol_report.txt'
+    @error_logger.create_error_data_file(file_date, file_name)
+    assert @error_logger.error_data_file_exists?(file_date, file_name)
+  end
+
+  def teardown
+    file_date = Date.new(2004, 10, 24).strftime("%m_%d_%Y")
+    file_name = 'nil_symbol_report.txt'
+    file = "#{file_date}_#{file_name}"
+    path_to_file = File.join(Rails.root, 'log', 'custom_log', "#{file}")
+    File.delete(path_to_file) if File.exist?(path_to_file)
   end
 end
