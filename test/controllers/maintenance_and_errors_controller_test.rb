744c25c0d3b0e236b6e5ac761d328aca44f1405c
Michael Becco <e4e6d4d5@gmail.com>
2019-01-14 09:10:55 -0800

    sign_in user to validate test

--- test/controllers/maintenance_and_errors_controller_test.rb
+++ test/controllers/maintenance_and_errors_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class MaintenanceAndErrorsControllerTest < ActionDispatch::IntegrationTest
   test "should get invalid_symbols" do
+    sign_in users(:admin)
     get maintenance_and_errors_invalid_symbols_url
     assert_response :success
   end
