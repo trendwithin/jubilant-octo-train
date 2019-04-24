7cb2dc90e1ce9edea4a5d1df87772fdc610e1370
Michael Becco <e4e6d4d5@gmail.com>
2019-04-24 10:48:10 -0700

    refactor to remove turbolinks issue

--- app/javascript/packs/barchart.jsx
+++ app/javascript/packs/barchart.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Barchart from 'ProjectRelatedHighCharts'
 
-document.addEventListener('turbolinks:load', function () {
-  var element = document.getElementById('barchart');
-  ReactDOM.render(<Barchart />, element);
-});
+ReactDOM.render(
+  <Barchart />, document.getElementById('barchart')
+);
--- app/javascript/packs/momentum_universe_chart.jsx
+++ app/javascript/packs/momentum_universe_chart.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MomentumUniverseLineChart from 'LineCharts/MomentumUniverse';
 
-document.addEventListener('turbolinks:load', function () {
-  var element = document.getElementById('momentum_universe_chart');
-  ReactDOM.render(<MomentumUniverseLineChart />, element);
-})
+// document.addEventListener('turbolinks:load', function () {
+//   var element = document.getElementById('momentum_universe_chart');
+//   ReactDOM.render(<MomentumUniverseLineChart />, element);
+// })
+
+ReactDOM.render(
+  <MomentumUniverseLineChart />, document.getElementById('momentum_universe_chart')
+);
